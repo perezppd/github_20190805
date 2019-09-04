644f4b6545844321a9e3a760eab0979c64ec0634
perezppd <perez.pd@hotmail.com>
2019-09-03 23:36:03 -0500

    print.py Version 0.2

--- print.py
+++ print.py
@@ -1,1 +1,8 @@
-print("Python program")
+print("New Python program version 1.1")
+drink=(input("Quieres una cerveza?"))
+print(drink)
+print(drink.upper())
+if drink.upper()=="YES" or "SI":
+	print("Aqui esta tu cerveza!")
+else:
+	print("No hay cerveza para ti")
